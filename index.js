abbe37d189409038daafa9e0b4fc2e3b21e66912
unknown <rahataminulislam37@gmail.com>
2022-11-27 12:23:22 +0600

    jwt added

--- index.js
+++ index.js
@@ -88,6 +88,18 @@ async function run() {
             const users = await usersCollection.find(query).toArray();
             res.send(users)
         })
+
+        app.get('/jwt', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const user = await usersCollection.findOne(query)
+            // if (user) {
+            //     const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, { expiresIn: '1h' })
+            //     return res.send({ accessToken: token })
+            // }
+            console.log(user);
+            res.status(403).send({ acccessToken: '' })
+        })
     }
 
     finally {
